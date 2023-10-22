c51da74d6c1befbc6f5f996db313bbe73b9f79f9
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-23 01:43:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import './Condition'
+import Condition from './Condition';
+import Call from './Call';
+
+function App() {
+  const h2 ='md:text-5xl text-2xl font-bold text-center py-5 uppercase text-slate-700';
+  const student=['nerob','mujahid','mahmudul','samim','sabbir','alamgir']
+  return (
+
+    <>
+      {/* <h2 className={`${h2}`}>conditional rendering prictice</h2>
+      <Condition name="react" state={true} next="jsx"></Condition>
+      <Condition name="react" state={false} next="jsx"></Condition>
+      <Condition name="react" state={true} next="jsx"></Condition>
+      <Condition name="react" state={false} next="jsx"></Condition>
+      <Condition name="react" state={true} next="jsx"></Condition> */}
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Condition.jsx
@@ -0,0 +1,109 @@
+// conditional rendering 01
+
+// export default function Condition({ name, state, next }) {
+//   const h2 =
+//     "md:text-xl text-lg font-bold text-center py-5 uppercase text-slate-700";
+
+//   if (state === true) {
+//     return (
+//       <div className="bg-slate-200 text-center w-6/12 mx-auto">
+//         <h2 className={`${h2}`}>course name : {name}</h2>
+//         <h2 className={`${h2}`}>Now learn : {next}</h2>
+//       </div>
+//     );
+//   }
+//   else
+//     return(
+//        <>
+//          <h2 className={`${h2}`}>course name : {name}</h2>
+//         <h2 className={`${h2}`}>continue learn : {name}</h2>
+
+//        </>
+//     )
+
+// }
+
+// conditional rendering 02
+// export default function Condition({ name, state, next }) {
+//     const h2 =
+//       "md:text-xl text-lg font-bold text-center py-5 uppercase text-slate-700";
+
+//     if (state) {
+//       return (
+//         <div className="bg-slate-200 text-center w-6/12 mx-auto mb-5">
+//           <h2 className={`${h2}`}>course name : {name}</h2>
+//           <h2 className={`${h2}`}>Now learn : {next}</h2>
+//         </div>
+//       );
+//     }
+//       return(
+//          <div className="bg-slate-300 text-center w-6/12 mx-auto mb-5">
+//             <h2 className={`${h2}`}>course name : {name}</h2>
+//           <h2 className={`${h2}`}>continue learn : {name}</h2>
+//          </div>
+//       )
+
+//   }
+
+// conditional rendering 03 with ternary oparetor
+// export default function Condition({ name, state }) {
+//     const h2 =
+//       "md:text-xl text-lg font-bold text-center py-5 uppercase text-slate-700";
+
+//       return(
+//          <div className="bg-slate-300 text-center w-6/12 mx-auto mb-5">
+//             <h2 className={`${h2}`}> {state ? 'Finshed learn': 'continue learn'}:{name}</h2>
+//          </div>
+//       )
+
+//   }
+
+// conditional rendering 04 with && -----jodi condition true hoi
+// export default function Condition({ name, state }) {
+//     const h2 ="md:text-xl text-lg font-bold text-center py-5 uppercase text-slate-700";
+
+//       return(
+//          <div className="bg-slate-300 text-center w-6/12 mx-auto mb-5">
+//             <h2 className={`${h2}`}>{name} {state && '- complitly leran'}</h2>
+//          </div>
+//       )
+
+//   }
+
+// conditional rendering 05 with || -----jodi condition true hoi
+// export default function Condition({ name, state }) {
+//     const h2 ="md:text-xl text-lg font-bold text-center py-5 uppercase text-slate-700";
+
+//       return(
+//          <div className="bg-slate-300 text-center w-6/12 mx-auto mb-5">
+//             <h2 className={`${h2}`}>{state || 'contineu leran -'} {name}</h2>
+//          </div>
+//       )
+
+//   }
+
+// conditional rendering 06 with variable
+export default function Condition({ name, state, next }) {
+  const h2 ="md:text-xl text-lg font-bold text-center py-5 uppercase text-slate-700";
+
+  let data;
+
+  if (state) {
+    data = (
+      <div className="bg-slate-200 text-center w-6/12 mx-auto mb-5">
+        <h2 className={`${h2}`}>course name : {name}</h2>
+        <h2 className={`${h2}`}>Now learn : {next}</h2>
+      </div>
+    );
+  } 
+    else {
+    data = (
+      <div className="bg-slate-300 text-center w-6/12 mx-auto mb-5">
+        <h2 className={`${h2}`}>course name : {name}</h2>
+        <h2 className={`${h2}`}>continue learn : {name}</h2>
+      </div>
+    );
+  }
+
+  return data;
+}
