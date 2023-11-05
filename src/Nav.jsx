a94b92204503d53831ac5081e675d3e772a113b6
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-11-06 00:19:46 +0600

    try to solve nav item

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,14 @@
 
 export default function Nav() {
     return (
-        <ul className=" justify-end gap-6 h-full bg-slate-500 text-white px-5">
+        <ul className=" h-full justify-end gap-6 w-10/12 mx-auto bg-slate-500 text-white px-5">
+            <li><a href="#"></a>class </li>
+            <li><a href="#"></a>class </li>
+            <li><a href="#"></a>class </li>
+            <li><a href="#"></a>class </li>
+            <li><a href="#"></a>class </li>
+            <li><a href="#"></a>class </li>
+            <li><a href="#"></a>class </li>
             <li><a href="#"></a>class </li>
             <li><a href="#"></a>class </li>
             <li><a href="#"></a>class </li>
