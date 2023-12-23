9bcb719ab44169cd2f77faf13961fb5a85c3d4f7
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-12-24 00:19:31 +0600

    pagination compleate

--- src/PaginatedData.jsx
+++ src/PaginatedData.jsx
@@ -1,34 +1,26 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-
-
-const ItemsPerPage = 10;
+const ItemsPerPage = 6;
 
 const PaginatedData = () => {
   const [currentPage, setCurrentPage] = useState(1);
-
-  const [data,setdata] = useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("../public/Data.json") // Use an absolute path or relative path to the public folder
+    // Assuming your data is stored in a file named Data.json in the public folder
+    fetch("/Data.json")
       .then((res) => res.json())
       .then((data) => {
-        setdata(data);
+        setData(data);
       })
-      .catch((error) => console.error('Error fetching data:', error));
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
-  
-
-  // Function to filter data based on designation
-  const filterDataByDesignation = (designation) => {
-    return data.filter((item) => item.designation === designation);
-  };
 
   // Function to get data for the current page
   const getCurrentPageData = () => {
     const startIndex = (currentPage - 1) * ItemsPerPage;
     const endIndex = startIndex + ItemsPerPage;
-    return filteredData.slice(startIndex, endIndex);
+    return data.slice(startIndex, endIndex);
   };
 
   // Event handler for next page
@@ -45,32 +37,43 @@ const PaginatedData = () => {
     }
   };
 
-  const designationToDisplay = "Engineer"; // Change this based on your requirement
-  const filteredData = filterDataByDesignation(designationToDisplay);
-  const totalPages = Math.ceil(filteredData.length / ItemsPerPage);
+  const totalPages = Math.ceil(data.length / ItemsPerPage);
   const currentPageData = getCurrentPageData();
 
   return (
-    <div>
-      <h1>{`Page ${currentPage} of ${totalPages}`}</h1>
+    <div className=" p-3">
+    <h2 className=" text-2xl text-black text-center mb-2">pagination</h2>
+    <h2 className=" text-2xl text-black text-center mb-2">{`Page ${currentPage} of ${totalPages}`}</h2>
 
-      {currentPageData.map((item) => (
-        <div key={item.id}>
-          <div className=" flex flex-col items-center bg-slate-200 rounded-lg p-3 shadow-inner text-lg font-semibold ">
-            <li key={item.id}>{item.name}</li>
-            <li key={item.id}>{item.technology}</li>
-            <li key={item.id}>{item.email}</li>
-            <li key={item.id}>{item.phone}</li>
+      <div className=" grid grid-cols-4 gap-4 mb-10">
+        {currentPageData.map((item) => (
+          <div key={item._id}>
+            <div className="flex flex-col items-center bg-slate-200 rounded-lg p-3 shadow-inner text-lg font-semibold">
+              <li key={item._id}>{item.name}</li>
+              <li key={item._id}>{item.technology}</li>
+              <li key={item._id}>{item.email}</li>
+              <li key={item._id}>{item.phone}</li>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
 
-      <button onClick={prevPage} disabled={currentPage === 1}>
-        Previous Page
-      </button>
-      <button onClick={nextPage} disabled={currentPage === totalPages}>
-        Next Page
-      </button>
+      <div className=" w-8/12 mx-auto flex gap-5 items-center justify-center ">
+        <button
+          className=" bg-lime-300 p-2 my-5 rounded-lg"
+          onClick={prevPage}
+          disabled={currentPage === 1}
+        >
+          Previous Page
+        </button>
+        <button
+          className=" bg-lime-300 p-2 my-5 rounded-lg"
+          onClick={nextPage}
+          disabled={currentPage === totalPages}
+        >
+          Next Page
+        </button>
+      </div>
     </div>
   );
 };
