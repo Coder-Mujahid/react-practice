8e9fa9724ac4e08335f203f558365a78114666a5
mujahidtemp404 <mujahidislam400301@gmail.com>
2024-01-03 00:44:37 +0600

    make  AgeCalculator interective and responsive

--- src/AgeCalculator.jsx
+++ src/AgeCalculator.jsx
@@ -46,36 +46,37 @@ const AgeCalculator = () => {
   const age = calculateAge();
 
   return (
-    <div className="max-w-2xl mx-auto">
-      <div className="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 p-8">
-        <input
-          type="date"
-          className="w-full mb-4 p-2 border rounded"
-          value={birthDate}
-          onChange={(e) => setBirthDate(e.target.value)}
-        />
-        <button
-          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-          onClick={calculateAge}
-        >
-          Calculate Age
-        </button>
-        {/* Display result only if birthDate is set */}
-        {birthDate && (
-          <div className="text-lg font-semibold mt-4">
-            <p>Your age is:</p>
-            <ul>
-              <li>{age.years} years</li>
-              <li>{age.months} months</li>
-              <li>{age.weeks} weeks</li>
-              <li>{age.days} days</li>
-              <li>{age.hours} hours</li>
-              <li>{age.seconds} seconds</li>
-            </ul>
-          </div>
-        )}
-      </div>
+    <div className="flex items-center justify-center h-screen">
+    <div className="bg-white shadow-lg border-[1px] border-slate-200 shadow-emerald-200 rounded-lg max-w-md p-8">
+    <h2 className=' text-center mb-5 text-2xl font-semibold capitalize'> calculate your age 👶 to 👴</h2>
+      <input
+        type="date"
+        className="w-full mb-4 p-2 border rounded focus:outline-none focus:border-blue-500"
+        value={birthDate}
+        onChange={(e) => setBirthDate(e.target.value)}
+      />
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
+        onClick={calculateAge}
+      >
+        Calculate Age
+      </button>
+      {birthDate && (
+        <div className="text-lg font-semibold mt-4">
+          <p>Your age is:</p>
+          <ul>
+            <li>{age.years} years</li>
+            <li>{age.months} months</li>
+            <li>{age.weeks} weeks</li>
+            <li>{age.days} days</li>
+            <li>{age.hours} hours</li>
+            <li>{age.minutes} minutes</li>
+            <li>{age.seconds} seconds</li>
+          </ul>
+        </div>
+      )}
     </div>
+  </div>
   );
 };
 
