b1fe8ca1e4071385880b91cdead8bea719d09702
mujahidtemp404 <mujahidislam400301@gmail.com>
2024-03-27 22:18:51 +0600

    try to add coin tosser bg

--- src/CoinToss.jsx
+++ src/CoinToss.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import bgr from '../public/img/money_6133156.png'; // Correct path to the image
 
 const CoinToss = () => {
   const [side, setSide] = useState(null);
@@ -9,20 +10,19 @@ const CoinToss = () => {
   };
 
   return (
-    <div>
     <div className="w-10/12 mx-auto flex flex-col items-center justify-center h-screen">
-    <h2 className='text-balck text-4xl font-bold mb-10'>CoinToss</h2>
+      <h2 className="text-black text-4xl font-bold mb-10">CoinToss</h2>
 
-      <div className="w-32 h-32 rounded-full border-4 border-gray-700 flex items-center justify-center mb-4 ">
+      <div className="w-32 h-32 bg-black bg-[url('{bgr}')] rounded-full border-4 border-gray-700 flex items-center justify-center mb-4">
         {side && <span className="text-4xl">{side}</span>}
       </div>
+
       <button
         className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700"
         onClick={tossCoin}
       >
         Toss Coin
       </button>
-    </div>
     </div>
   );
 };
