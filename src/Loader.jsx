0f37d5a82b9f0501662fbdb6019ffad72c30a304
mujahidtemp404 <mujahidislam400301@gmail.com>
2024-04-19 23:01:50 +0600

    try to add awosome loder formy project

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,9 +1,24 @@
+
 import React from 'react';
 
 const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+      <div className="animate-bounce">
+        <svg
+          className="w-16 h-16 text-gray-900"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M3 21a9 9 0 010-18 9 9 0 016.84 14.89l.33.33M22 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 };
